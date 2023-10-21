1f3ba0604607f831bf79cf853b468d76b8ba7633
William Bigas Mauro <william.mauro@ibm.com>
2023-10-21 16:55:44 -0300

    Event binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -11,8 +11,20 @@ export class DataBidingComponent {
   public idade: number = 25;
   public maisUm: number = 1;
 
+  public position: { x: number, y: number } = {x: 0, y: 0};
+
   public imgSrc: string = "https://enotas.com.br/blog/wp-content/uploads/2021/02/GitHub.jpg";
 
   public checkedDisable: boolean = true;
   public imgTitle: string = "Property binding";
+
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor)
+  }
+
+  public mouseMoveTest(valor: MouseEvent) {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
