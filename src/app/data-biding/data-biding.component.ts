cf136c3f0e7bca869bc028344d069a907635aaed
William Bigas Mauro <william.mauro@ibm.com>
2023-10-21 16:37:32 -0300

    Property binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -10,4 +10,9 @@ export class DataBidingComponent {
   public nome: string = "William";
   public idade: number = 25;
   public maisUm: number = 1;
+
+  public imgSrc: string = "https://enotas.com.br/blog/wp-content/uploads/2021/02/GitHub.jpg";
+
+  public checkedDisable: boolean = true;
+  public imgTitle: string = "Property binding";
 }
