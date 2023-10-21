074c271e9c019f01adbccdf4120a94168592ada8
William Bigas Mauro <william.mauro@ibm.com>
2023-10-21 18:40:18 -0300

    ngContent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,19 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<!--    <app-data-biding></app-data-biding>-->
-<!--    <app-diretivas-estruturais></app-diretivas-estruturais>-->
-    <app-diretivas-atributos></app-diretivas-atributos>
-    <router-outlet></router-outlet>
+      <!--    <app-data-biding></app-data-biding>-->
+      <!--    <app-diretivas-estruturais></app-diretivas-estruturais>-->
+
+      <app-diretivas-atributos>
+          <h1>Aulas de Diretivas Atributos</h1>
+          <h3>Final da Aula</h3>
+      </app-diretivas-atributos>
+
+      <app-diretivas-atributos>
+          <h1>William Bigas Mauro</h1>
+          <h3>Railane Cardoso</h3>
+      </app-diretivas-atributos>
+      <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
