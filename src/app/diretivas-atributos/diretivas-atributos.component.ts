a7cc11e875557b87f2c21fea73217d8bd482a209
William Bigas Mauro <william.mauro@ibm.com>
2023-10-21 18:05:08 -0300

    ngStyle

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -8,10 +8,20 @@ import {Component, OnInit} from '@angular/core';
 export class DiretivasAtributosComponent implements OnInit {
 
   public valor: boolean = true;
+  public altura: string = "20px";
+  public backgroundColor: string = "red";
 
   ngOnInit(): void {
     setInterval(() => {
       this.valor = !this.valor;
+
+      if (this.altura == "20px") {
+        this.altura = "50px";
+        this.backgroundColor = "blue";
+      } else {
+        this.altura = "20px";
+        this.backgroundColor = "red";
+      }
     }, 2000)
 
 
