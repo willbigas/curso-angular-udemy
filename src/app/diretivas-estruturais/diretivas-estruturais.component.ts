c0e06466914e0993cfb216d4a88a948c26e397aa
William Bigas Mauro <william.mauro@ibm.com>
2023-10-21 17:43:43 -0300

    ngfor

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -9,11 +9,16 @@ export class DiretivasEstruturaisComponent implements OnInit {
   public condition: boolean = true;
   public conditionClick: boolean = true;
   public elseBlock: any;
+  public list: Array<{ nome: string , idade: number }> =
+    [{nome: "William Bigas Mauro" , idade : 25},
+      {nome: "Gabriel Bigas Mauro" , idade: 59},
+      {nome: "Wellington Bigas", idade: 55}
+    ];
 
   ngOnInit(): void {
     setInterval(() => {
       if (this.condition) {
-         this.condition = false;
+        this.condition = false;
       } else {
         this.condition = true;
       }
@@ -21,11 +26,18 @@ export class DiretivasEstruturaisComponent implements OnInit {
   }
 
   public onClick() {
-     if (this.conditionClick) {
-         this.conditionClick = false;
-      } else {
-        this.conditionClick = true;
-      }
+    if (this.conditionClick) {
+      this.conditionClick = false;
+    } else {
+      this.conditionClick = true;
+    }
   }
 
+  public onClickAddList() {
+    this.list.push({nome: "Railane" , idade: 28})
+  }
+
+  public onClickEventList(event: number) {
+    this.list.splice(event, 1)
+  }
 }
