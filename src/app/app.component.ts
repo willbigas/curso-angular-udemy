bc4e1572ce188cfd2b7ac9e06e408220570e2d09
William Bigas Mauro <william.mauro@ibm.com>
2023-10-22 11:23:13 -0300

    Recebendo dados do componente filho por notificação de evento e realizando processamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,40 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <!--    <app-data-biding></app-data-biding>-->
-      <!--    <app-diretivas-estruturais></app-diretivas-estruturais>-->
+    <!--    <app-data-biding></app-data-biding>-->
+    <!--    <app-diretivas-estruturais></app-diretivas-estruturais>-->
 
-<!--      <app-diretivas-atributos>-->
-<!--          <h1>Aulas de Diretivas Atributos</h1>-->
-<!--          <h3>Final da Aula</h3>-->
-<!--      </app-diretivas-atributos>-->
+    <!--      <app-diretivas-atributos>-->
+    <!--          <h1>Aulas de Diretivas Atributos</h1>-->
+    <!--          <h3>Final da Aula</h3>-->
+    <!--      </app-diretivas-atributos>-->
 
-<!--      <app-diretivas-atributos>-->
-<!--          <h1>William Bigas Mauro</h1>-->
-<!--          <h3>Railane Cardoso</h3>-->
-<!--      </app-diretivas-atributos>-->
+    <!--      <app-diretivas-atributos>-->
+    <!--          <h1>William Bigas Mauro</h1>-->
+    <!--          <h3>Railane Cardoso</h3>-->
+    <!--      </app-diretivas-atributos>-->
 
-<!--      <app-diretivas-atributos></app-diretivas-atributos>-->
+    <!--      <app-diretivas-atributos></app-diretivas-atributos>-->
 
-<!--      <app-new-component></app-new-component>-->
+    <!--      <app-new-component></app-new-component>-->
 
-<!--      <app-input [contador]="addValue"></app-input>-->
-<!--      <br>-->
-<!--      <button (click)="add()"> Add </button>-->
+    <!--      <app-input [contador]="addValue"></app-input>-->
+    <!--      <br>-->
+    <!--      <button (click)="add()"> Add </button>-->
 
-
-      <app-output></app-output>
-      <router-outlet></router-outlet>
+    <ng-template [ngIf]="getDados">
+      <h1>{{getDados.nome}}</h1>
+      <h1>{{getDados.idade}}</h1>
+    </ng-template>
+    <app-output (enviarDados)="setDados($event)"></app-output>
+    <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
 
   public addValue: number = 0;
+
+  public getDados: { nome: string, idade: number } | undefined;
 
   constructor() {
   }
@@ -43,5 +48,9 @@ export class AppComponent implements OnInit {
 
   public add() {
     this.addValue += 1;
+  }
+
+  public setDados(event: { nome: string, idade: number }) {
+    this.getDados = event;
   }
 }
