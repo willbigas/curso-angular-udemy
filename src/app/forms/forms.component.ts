c775ba253aafefee64915e8d295d35cc62bc40ee
William Bigas Mauro <william.mauro@ibm.com>
2023-10-28 17:58:08 -0300

    Validacao de ngform

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-forms',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
+
+  public listComidas: Array<{ comida: string, preco: string }> = [
+    {comida: "X-Salada", preco: "RS 10,00"},
+    {comida: "X-Bacon", preco: "RS 15,00"},
+    {comida: "Coxinha", preco: "RS 20,00"}
+  ]
+
 
 }
